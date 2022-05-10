68695adb86b45291c87f3c9a134ed65b4a7248fb
Harshan R <104904345+Harshan-R@users.noreply.github.com>
2022-05-10 21:06:19 +0530

    changed by Harshan

--- miniproject.sql
+++ miniproject.sql
@@ -11,3 +11,20 @@ create table users (
 	joined_from date default current_date,
 	constraint pk_user primary key (username) 
 );
+create table tweet (
+	tweetid integer,
+	content_ varchar(100),
+	photo text,
+	time_ time ,
+	author varchar(20),
+	constraint fk_author foreign key (author) references users (username), 
+	constraint pk_tweet primary key (tweetid) 
+);
+
+create table follows(
+    follower varchar(20),
+	followee varchar(20),
+	constraint fk_follower foreign key (follower)references users (username),
+	constraint fk_followee foreign key (followee)references users (username),
+	constraint pk_follwer_followee primary key(follower,followee)
+)
