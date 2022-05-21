7838c7fff461546dbbea84fdf4c7a54dd7005552
Akash S P <AkashSCIENTIST@users.noreply.github.com>
2022-05-21 20:55:41 +0530

    Update rest.py

--- rest.py
+++ rest.py
@@ -3,7 +3,9 @@ from demo import *
 #from embeddedsql import *
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+'''Prelims''' 
+
+@app.route('/', methods=['GET'])
 def home():
 	return '''
     Available methods are :
@@ -22,22 +24,19 @@ def home():
     /new_like
     '''
 
-@app.route('/feed/<user>', methods = ['GET'])
+@app.route('/feed/<user>', methods=['GET'])
 def feed(user):
     data = "Feed page goes here"
     return jsonify({"data":data})
 
-@app.route('/user/<user>', methods = ['GET'])
+'''User Table'''
+
+@app.route('/user/<user>', methods=['GET'])
 def user(user):
     data = ["User {} page goes here".format(user)]
     return jsonify({"data":data})
 
-@app.route('/tweet/<int:tweetid>', methods = ['GET'])
-def tweet(tweet_id):
-    data = ["Tweet of id {} goes here".format(tweet_id)]
-    return jsonify({"data":data})
-
-@app.route('/new_user', methods = ['POST'])
+@app.route('/new_user', methods=['POST'])
 def new_user():
     data = request.form
     files = request.files
@@ -49,52 +48,10 @@ def new_user():
     newUser(**out)
     return redirect(url_for('home'))
 
-@app.route("/new_like", methods=['POST'])
-def new_like():
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
     data = request.form
-    tweet_id = data['tweet_id']
-    user_id = data['user_id']
-    return jsonify({"data":data})
-
-@app.route("/new_comment", methods=['POST'])
-def new_comment():
-    data = request.form
-    print(dict(data))
-    tweet_id = data['tweet_id']
-    user_id = data['user_id']
-    content = data['content']
-    return jsonify({"data":data})
-
-@app.route('/new_follow', methods=['POST'])
-def new_follow():
-    data = request.form
-    print(data)
-    follower = data['follower']
-    followee = data['followee']
-    return jsonify({"data":data})
-
-@app.route("/new_unlike", methods=['POST'])
-def new_unlike():
-    data = request.form
-    tweet_id = data['tweet_id']
-    user_id = data['user_id']
-    return jsonify({"data":data})
-
-@app.route("/delete_comment", methods=['POST'])
-def delete_comment():
-    data = request.form
-    print(dict(data))
-    tweet_id = data['tweet_id']
-    user_id = data['user_id']
-    content = data['content']
-    return jsonify({"data":data})
-
-@app.route('/new_unfollow', methods=['POST'])
-def new_unfollow():
-    data = request.form
-    print(data)
-    follower = data['follower']
-    follows = data['follows']
+    username = data['username']
     return jsonify({"data":data})
 
 @app.route('/change_photo', methods=['GET'])
@@ -109,16 +66,98 @@ def change_photo():
     }
     return jsonify({"data":out})
 
-@app.route('/new_tweet', methods=['GET'])
-def new_tweet():
-    data = request.form
-    files = request.files
-    image = dict(files)['image']
-    out = {
-        'data':data,
-        'image':image
-    }
-    return jsonify({"data":out})
+'''Follower Table'''
 
-if __name__ == '__main__':
-	app.run()
+@app.route('/new_follow', methods=['POST'])
+def new_follow():
+    data = request.form
+    print(data)
+    follower = data['follower']
+    followee = data['follows']
+    return jsonify({"data":data})
+
+@app.route('/new_unfollow', methods=['POST'])
+def new_unfollow():
+    data = request.form
+    print(data)
+    follower = data['follower']
+    follows = data['follows']
+    return jsonify({"data":data})
+
+'''Like Table'''
+
+@app.route("/new_like", methods=['POST'])
+def new_like():
+    data = request.form
+    tweet_id = data['tweet_id']
+    user_id = data['user_id']
+    #time = time
+    return jsonify({"data":data})
+
+@app.route("/new_unlike", methods=['POST'])
+def new_unlike():
+    data = request.form
+    tweet_id = data['tweet_id']
+    user_id = data['user_id']
+    #time = time
+    return jsonify({"data":data})
+
+'''Message Table'''
+
+@app.route("/new_message", methods=['POST'])
+def new_message():
+    data = request.form
+    sender = data['sender']
+    receiver = data['receiver']
+    content = data['message']
+    data['time'] = 10
+    return jsonify({"data":data})
+
+@app.route("/delete_message", methods=['POST'])
+def new_message():
+    data = request.form
+    sender = data['sender']
+    receiver = data['receiver']
+    time = data['time']
+    return jsonify({"data":data})
+
+'''Group Member Table'''
+
+@app.route('/new_group_member', methods=['POST'])
+def new_group_member():
+    data = request.form
+    group_name = data['group_name']
+    username = data['username']
+    return jsonify({"data":data})
+
+@app.route('/delete_group_member', methods=['POST'])
+def delete_group_member():
+    data = request.form
+    group_name = data['group_name']
+    username = data['username']
+    return jsonify({"data":data})
+
+'''Vote Table'''
+
+@app.route('/cast_vote', methods=['POST'])
+def cast_vote():
+    data = request.form
+    username = data['username']
+    poll_id = data['poll_id']
+    return jsonify({"data":data})
+
+'''Poll Option Table'''
+
+@app.route('/new_poll_option', methods=['POST'])
+def new_poll_option():
+    data = request.form
+    poll_id = data['poll_id']
+    option = data['option']
+    return jsonify({"data":data})
+
+'''Poll Posted By Table'''
+
+@app.route('/poll_posted_by', methods=['POST'])
+def new_poll():
+    data = request.form
+    return jsonify({"data":data})
